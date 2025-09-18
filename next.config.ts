6b8cd763ee80967d4405382082ba48550e5a7c6a
reagin <reagin@163.com>
2025-09-18 10:56:20 +0800

    init: initialize repository

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  allowedDevOrigins: ['localhost'],
+  devIndicators: false,
+  distDir: 'build',
+};
+
+export default nextConfig;
